ddfed11cd319e9ad1ee0a17c3b73975e8b416d44
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2021-01-23 22:18:07 +0530

    Login functionality implemented

--- Login.java
+++ Login.java
@@ -1,5 +1,15 @@
 class Login{
 	public static void main(String args[]){
 		System.out.println("Login functionality has to be developed");
+		String username ="admin";
+		String password ="admin";
+		verifyLogin(username,password)
+	}
+
+	public boolean verifyLogin(String username, String password){
+		if(username.equals("admin")&& password.equals("admin")){
+		return true;
+	}
+	return false;
 	}
 }
